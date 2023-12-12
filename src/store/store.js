1e9eb614179d6ed385a579a2ca393f7d80c58f6c
JurbaDaria <126083934+JurbaDaria@users.noreply.github.com>
2023-12-13 00:04:01 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ export const fetchTasks = createAsyncThunk(
   'tasks/fetchTasks',
   async () => {
     try {
-      const response = await axios.get('/api/tasks'); 
+      const response = await axios.get('/store/tasks'); 
       return response.data; 
     } catch (error) {
       throw Error('Failed to fetch tasks'); 
